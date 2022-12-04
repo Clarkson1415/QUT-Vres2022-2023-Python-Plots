851dcca7aadb567c3ab6bd83e8c76842086acd7a
Clarkson <graceclarkson1415@gmail.com>
2022-12-04 11:44:26 +1000

    plots in loops yay

--- M_plot.py
+++ M_plot.py
@@ -1,1 +1,22 @@
-M_plot = plt.plot(t, M, 'b')
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import math
+
+
+# M plot - solution
+K = 1
+O = 1
+Lambda = 1.4
+
+t = np.linspace(0,5)
+
+M_0 = 1
+# plot T = 0
+
+for T in range(2): # for T = 0, 1, 2
+    c = K * math.log((O * T - K / (Lambda * M_0)) + 1) / (O * T - Lambda * K)
+    M = (O*T-Lambda*K)/(Lambda*(np.exp((t+c)*(O*T-Lambda*K)/K)-1))
+    plt.plot(t, M, 'b', label=f'T={T}')
+    plt.legend()
+plt.show()
--- dmdt_plot.py
+++ dmdt_plot.py
@@ -7,21 +7,14 @@ K = 1
 O = 1.1
 T = 0
 Lambda = 1.4
-#c = K * math.log((O * T / ( Lambda * M)) - K +1, e) / (O * T - Lambda * K)
 
-M0 = np.linspace(0, 2)
-#dmdt = Lambda*M*(1-(M/K)) - O*M*T
+M = np.linspace(0, 1)
 
-dmdt = (Lambda*M0) - ((Lambda*M0*M0)/K) - (O*M0*T)
-dmdt_plot = plt.plot(M0, dmdt, 'r-')
+for T in range(3): # for = 0, 1, 2
+    dmdt = (Lambda * M) - ((Lambda * M * M) / K) - (O * M * T)
+    plt.plot(M, dmdt, 'b-', label=f'T={T}')
+    plt.legend()
 plt.show()
 
-# M plot of solution
-t = np.linspace(0,20)
-c = 5
-M = (O*T-Lambda*K)/(Lambda*(np.exp((t+c)*(O*T-Lambda*K)/K)-1))
-M_plot = plt.plot(t, M, 'b')
-plt.show()
-
-# M plot of solution ODE int solver python to check
-
+# M plot of solution ODE int solver python to check same as M_plot
+dmdt = (Lambda*M) - ((Lambda*M*M)/K) - (O*M*T)
