f3b2503fe6d841da405a0e282b1171f8ae0d918d
Clarkson <graceclarkson1415@gmail.com>
2022-12-26 11:48:00 +1000

    m plot is sol but not matching why

--- M_plot.py
+++ M_plot.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-M0 = 2
-K = 1
-Lambda = 1
-phi = 0
+M0 = 3
+K = 2
+Lambda = 5
+phi = 1
 T = 1
 # M = solution plot
 def dMdt(M, t):
@@ -14,19 +14,18 @@ def dMdt(M, t):
 
 
 # x axis = time
-t = np.linspace(0,2)
+t = np.linspace(0, 2)
 
 # solution?
-c = 1/Lambda * np.log(abs(M0/(Lambda*(M0-K))))
-My_sol = (K*(Lambda - T*phi)*np.exp((Lambda*(t+c)))) / (Lambda*np.exp((Lambda*(t+c))) - np.exp((T*phi*(t+c))))
+c = 1/(T*phi - Lambda) * np.log((K*(T*phi))/M0 + 1)
+the_sol = (K*(Lambda - T*phi)*np.exp(Lambda*(t+c))) / (-np.exp(T*phi*(t+c)) + Lambda*np.exp(Lambda*(t+c)))
 
-c2 = np.log(abs((K*M0)/(K*(Lambda-phi*T) - Lambda*M0)) / (Lambda - phi*T))
-sol2 = (K*np.exp((t+c2)*(Lambda - phi*T))) / (K + Lambda*np.exp((t+c2)*(Lambda - phi*T)))
-
+#c2 = 1/(Lambda - phi*T) * np.log((K*M0)/(Lambda-phi*T*K-M0*K))
+#mysol = (K*np.exp((t+c2)*(Lambda-phi*T))*(Lambda - phi*T))/(K+Lambda*np.exp((t+c2)*(Lambda-phi*T)))
 
 M = odeint(dMdt, M0, t)
 plt.plot(t, M, label=f'sol ode', linewidth=6)
-plt.plot(t, sol2, label=r"$M(t)=\frac{K(\lambda - T\phi)e^{\lambda(t+c)}}{\lambda e^{\lambda(t+c)} - e^{T\phi(t+c)}}$")
+plt.plot(t, the_sol, label=r"$M(t)=\frac{K(\lambda - T\phi)e^{\lambda(t+c)}}{\lambda e^{\lambda(t+c)} - e^{T\phi(t+c)}}$")
 plt.legend()
 plt.title('M(t)')
 plt.xlabel('t')
