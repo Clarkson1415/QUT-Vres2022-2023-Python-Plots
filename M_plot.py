9c2201a14c61f2c519b4fcfaebf74cc825106d66
Clarkson <graceclarkson1415@gmail.com>
2023-01-04 07:49:50 +1000

    no conflics

--- M_plot.py
+++ M_plot.py
@@ -31,9 +31,34 @@ M = odeint(dMdt, M0, t)
 plt.plot(t, M, label=f'sol ode', linewidth=6)
 plt.plot(t, the_sol_wolf, label=r"wolfram")
 plt.plot(t, mysol, label=r"mysol")
-plt.legend()
-plt.title('M(t)')
-plt.xlabel('t')
+#
+# M0 = 1
+# K = 1
+# Lambda = 1
+# phi = 1
+# T = 0
+# #dMdt = (Lambda * M) - ((Lambda * M * M) / K) - (phi * M * T)
+# dMdt = Lambda*M*(1 - (M/K)) - (phi*M*T)
+# return dMdt
+#
+# # x axis = time
+# t = np.linspace(0,2)
+# # solution
+# c = 1/Lambda * np.log(abs(M0/(Lambda*(M0-K))))
+# My_sol = (K*(Lambda - T*phi)*np.exp((Lambda*(t+c)))) / (Lambda*np.exp((Lambda*(t+c))) - np.exp((T*phi*(t+c))))
+#
+# c2 = 1/(Lambda-phi*T) * np.log(abs(K*M0 / (K*(Lambda-phi*T)-Lambda*M0)))
+# My_sol2 = (K*(Lambda-phi*T)*np.exp((t+c2)*(Lambda-phi*T))) / (K + Lambda * np.exp((t+c2)*(Lambda-phi*T)))
+#
+#
+# M = odeint(dMdt, M0, t)
+# plt.plot(t, M, label=f'ode solver', linewidth=6)
+# plt.plot(t, My_sol, label=r"$M(t)=\frac{K(\lambda - T\phi)e^{\lambda(t+c)}}{\lambda e^{\lambda(t+c)} - e^{T\phi(t+c)}}$")
+# plt.plot(t, My_sol2, label=r"$mine: M(t)=\frac{K(\lambda - T\phi)e^{\lambda(t+c)}}{\lambda e^{\lambda(t+c)} - e^{T\phi(t+c)}}$")
+# >>>>>>> Stashed changes
+# plt.legend()
+# plt.title('M(t)')
+# plt.xlabel('t')
 # plot with eulers method approx, euler(x0, y0, xn, steps)
 Eulers.euler(dMdt, 0,1,2,100)
 plt.show()
